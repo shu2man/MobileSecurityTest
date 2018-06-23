a6eaaf9abaf1afff03ce0fff2feda95cba6dbf9f
shu2man <570700160@qq.com>
2018-06-23 17:18:22 +0800

    目前实现了SharedPreference存储，后续增加File/SQL数据库存储

---
+++ app/src/main/java/com/example/hxs15/mobilesecuritytest/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.hxs15.mobilesecuritytest;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private String JWBUrl="http://jwb.sysu.edu.cn/";
+    private String JWXTUrl="http://uems.sysu.edu.cn/jwxt/";
+    private String SYSUUrl="http://www.sysu.edu.cn/2012/cn/index.htm";
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initWebView();
+    }
+
+    public void initWebView(){
+        progressBar=findViewById(R.id.main_web_progressbar);
+        webView=findViewById(R.id.main_web_view);
+        webView.setWebViewClient(webViewClient);
+        webView.loadUrl(JWBUrl);
+    }
+
+    public void changeWeb(View view){
+        int id=view.getId();
+        if(id==R.id.jwb_btn){
+            webView.loadUrl(JWBUrl);
+        }
+        else if(id==R.id.jwxt_btn){
+            webView.loadUrl(JWXTUrl);
+        }
+        else if(id==R.id.sysu_btn){
+            webView.loadUrl(SYSUUrl);
+        }
+    }
+
+
+
+    //WebViewClient主要帮助WebView处理各种通知、请求事件
+    private WebViewClient webViewClient=new WebViewClient(){
+        @Override
+        public void onPageFinished(WebView view, String url) {//页面加载完成
+            progressBar.setVisibility(View.GONE);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {//页面开始加载
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+/*            if(url.equals("http://www.google.com/")){
+                Toast.makeText(MainActivity.this,"国内不能访问google,拦截该url",Toast.LENGTH_LONG).show();
+                return true;//表示我已经处理过了
+            }
+            return super.shouldOverrideUrlLoading(view, url);*/
+            webView.loadUrl(url);
+            return true;
+        }
+
+    };
+}
---
+++ app/src/main/java/com/example/hxs15/mobilesecuritytest/SignInActivity.java
@@ -0,0 +1,165 @@
+package com.example.hxs15.mobilesecuritytest;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+/**
+ * Created by hxs15 on 2018-6-23.
+ */
+
+public class SignInActivity extends AppCompatActivity{
+    private SharedPreferences sharedPreferences;
+    private int SignMode=1;
+    private String defaultString="迹趣官方";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        initLogin();
+    }
+
+    public void initLogin(){
+        sharedPreferences=getApplicationContext().getSharedPreferences("MyPreference",MODE_PRIVATE);
+        String lastUser=sharedPreferences.getString("lastUser",null);
+        String lastPswd=sharedPreferences.getString("lastPswd",null);
+        if(!(TextUtils.isEmpty(lastUser) || TextUtils.isEmpty(lastPswd))){
+            EditText name=findViewById(R.id.login_name_edit);
+            EditText pswd=findViewById(R.id.login_password_edit);
+            name.setText(lastUser);
+            pswd.setText(lastPswd);
+        }
+    }
+
+    public void registerIn(View view){
+        login();
+    }
+
+    public void login(){
+        EditText name=findViewById(R.id.login_name_edit);
+        EditText pswd=findViewById(R.id.login_password_edit);
+        EditText cpsd=findViewById(R.id.confirm_password_edit);
+        if(SignMode==1){
+            if(!(TextUtils.isEmpty(name.getText()) || TextUtils.isEmpty(pswd.getText()))){
+                boolean isPsdRight=confirmUser(name.getText().toString(),pswd.getText().toString());
+                if(isPsdRight){
+                    SharedPreferences.Editor editor=sharedPreferences.edit();
+                    editor.putString("lastUser",name.getText().toString());
+                    editor.putString("lastPswd",pswd.getText().toString());
+                    editor.apply();
+                    goToMain(name.getText().toString(),pswd.getText().toString());
+                }
+                else{
+                    Toast.makeText(this,"用户名或密码错误",Toast.LENGTH_SHORT).show();
+                }
+            }
+            else if(TextUtils.isEmpty(name.getText()) && TextUtils.isEmpty(pswd.getText())){
+                Toast.makeText(this,"输入为空",Toast.LENGTH_SHORT).show();
+            }
+            else if(TextUtils.isEmpty(name.getText())){
+                name.setError("用户名为空");
+            }
+            else if(TextUtils.isEmpty(pswd.getText())){
+                pswd.setError("密码为空");
+            }
+        }
+        else if(SignMode==2){
+            if(!(TextUtils.isEmpty(name.getText()) || TextUtils.isEmpty(pswd.getText()) || TextUtils.isEmpty(cpsd.getText()))){
+                if(pswd.getText().toString().equals(cpsd.getText().toString())){
+                    String spname=sharedPreferences.getString(name.getText().toString(),defaultString);
+                    if (!spname.equals(defaultString)) {
+                        Toast.makeText(this,"用户名已经被使用啦！",Toast.LENGTH_SHORT).show();
+                    }
+                    else if(pswd.getText().toString().length()<8){
+                        Toast.makeText(this,"密码长度不能小于8哦",Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        SharedPreferences.Editor editor=sharedPreferences.edit();
+                        editor.putString("lastUser",name.getText().toString());
+                        editor.putString("lastPswd",pswd.getText().toString());
+                        editor.putString(name.getText().toString(),pswd.getText().toString());
+                        editor.apply();
+                        goToMain(name.getText().toString(),pswd.getText().toString());
+                    }
+                }
+                else{
+                    Toast.makeText(this,"密码不一致",Toast.LENGTH_SHORT).show();
+                }
+            }
+            else if(TextUtils.isEmpty(name.getText()) && TextUtils.isEmpty(pswd.getText()) && TextUtils.isEmpty(cpsd.getText())){
+                Toast.makeText(this,"输入为空",Toast.LENGTH_SHORT).show();
+            }
+            else if(TextUtils.isEmpty(name.getText())){
+                name.setError("用户名为空");
+            }
+            else if(TextUtils.isEmpty(pswd.getText())){
+                pswd.setError("密码为空");
+            }
+            else if(TextUtils.isEmpty(cpsd.getText())){
+                cpsd.setError("请确认密码");
+            }
+        }
+    }
+
+    public void goToMain(String name,String pswd){
+        Intent intent=new Intent(this,MainActivity.class);
+        intent.putExtra("user",name);
+        intent.putExtra("pswd",pswd);
+        startActivity(intent);
+        this.finish();
+    }
+
+    public boolean confirmUser(String name,String psd){
+        String spswd=sharedPreferences.getString(name,defaultString);
+        if(spswd.equals(psd)) return true;
+        else return false;
+    }
+
+    public void goToRegister(View view){
+        SignMode=2;
+        registerVisibility(2);
+    }
+
+    public void registerVisibility(int mode){//1-exist user login,  2-new user register
+        ConstraintLayout CL1=(ConstraintLayout)findViewById(R.id.login_alternative_container);
+        TextView tv=(TextView)findViewById(R.id.confirm_password_text);
+        TextView ltv=findViewById(R.id.login_has_count);
+        EditText et=(EditText)findViewById(R.id.confirm_password_edit);
+        EditText etname=(EditText)findViewById(R.id.login_name_edit);
+        Button btn=(Button)findViewById(R.id.login_btn);
+        if(mode==1){
+            CL1.setVisibility(View.VISIBLE);
+            tv.setVisibility(View.GONE);
+            ltv.setVisibility(View.GONE);
+            et.setVisibility(View.GONE);
+        }
+        else if(mode==2){
+            CL1.setVisibility(View.GONE);
+            tv.setVisibility(View.VISIBLE);
+            ltv.setVisibility(View.VISIBLE);
+            et.setVisibility(View.VISIBLE);
+            btn.setText("完成");
+        }
+    }
+
+    public void registerByWechat(View view){
+        Toast.makeText(this,"暂不支持，敬请期待！",Toast.LENGTH_SHORT).show();
+    }
+
+    public void fromRegister2Login(View view){
+        SignMode=1;
+        registerVisibility(1);
+    }
+}
